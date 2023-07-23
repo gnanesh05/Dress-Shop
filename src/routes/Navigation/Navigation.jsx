10cc2d3b837800e2ebb72462c2dbe34686dc1012
gnanesh05 <gnanesh007.mk@gmail.com>
2023-07-23 21:49:30 +0530

    added navbar

---
+++ src/routes/Navigation/Navigation.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Link, Outlet } from 'react-router-dom'
+import "./Navigation.styles.scss"
+import { ReactComponent as Logo } from '../../assests/crown.svg'
+
+const Navigation = ()=>{
+    return(
+      <>
+        <div className='navigation'>
+            <Link className='logo-container' to='/'>
+                <Logo className='logo'/>
+            </Link>
+            <div className="nav-links-container">
+                <Link className='nav-link' to='/shop'>
+                    SHOP
+                </Link>
+                <Link className='nav-link' to='/shop'>
+                    SHOP
+                </Link>
+                <Link className='nav-link' to='/shop'>
+                    SHOP
+                </Link>
+            </div>
+        </div>
+        <Outlet/>
+      </>
+    )
+  }
+
+export default Navigation
