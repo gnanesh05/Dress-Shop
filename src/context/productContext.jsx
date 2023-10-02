92b3df77a4228da1f53dc7573fa6a14092b24666
gnanesh05 <gnanesh007.mk@gmail.com>
2023-10-02 17:52:20 +0530

    loaded products data to fire storage

--- src/context/productContext.jsx
+++ src/context/productContext.jsx
@@ -1,12 +1,16 @@
-import { createContext, useState } from "react";
-import Products from '../assests/shop-data.json';
+import { createContext, useState, useEffect } from "react";
+import SHOP_DATA from '../assests/shop-data.js';
+import { addCollectionAndDocuments } from "../utils/firebase.js";
 
 export const ProductContext = createContext({
     products:[],
 });
 
 export const ProductProvider = ({children})=>{
-    const[products, setProducts] = useState(Products);
+    const[products, setProducts] = useState(SHOP_DATA);
+    // useEffect(()=>{
+    //     addCollectionAndDocuments('categories', SHOP_DATA);
+    // },[])
     const value = {products};
     return(
         <ProductContext.Provider value={value}>{children}</ProductContext.Provider>
--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -4,7 +4,7 @@ import {getAuth,signInWithRedirect,signInWithPopup,
      GoogleAuthProvider, createUserWithEmailAndPassword,
      signInWithEmailAndPassword, signOut, onAuthStateChanged
     } from 'firebase/auth';
-import {getFirestore, doc, getDoc, setDoc} from 'firebase/firestore';
+import {getFirestore, doc, getDoc, setDoc, collection, writeBatch} from 'firebase/firestore';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -30,6 +30,19 @@ export const signInWithGoogleRedirect = ()=>signInWithRedirect(auth, provider);
 
 //setting up db
 export const db = getFirestore();
+
+export const addCollectionAndDocuments = async(collectionKey,objectsToAdd)=>{
+    const collectionRef = collection(db,collectionKey);
+    const batch = writeBatch(db);
+
+    objectsToAdd.forEach((object)=>{
+        const docRef = doc(collectionRef,object.title.toLowerCase());
+        batch.set(docRef, object);
+    });
+
+    await batch.commit();
+    console.log('DONE');
+}
 export const createUserDocumentFromAuth = async(userAuth)=>{
     if(!userAuth)
      return;
